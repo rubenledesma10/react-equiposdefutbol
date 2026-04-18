1804e85142cb3cc669518c2cd768eb471af0903f
rubenledesma10 <rubenledesma89@gmail.com>
2026-04-18 17:30:13 -0300

    add styles navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import Navbar from './components/Navbar.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
@@ -9,7 +8,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Navbar />
-      <App />
+      
     </BrowserRouter>
 
 
