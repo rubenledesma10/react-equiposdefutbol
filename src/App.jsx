fbcae358c27ec54e135581ee7baf9b2a35ef4b04
Damaris. <168873502+Damaris-97@users.noreply.github.com>
2026-04-18 20:51:14 -0300

    componentes Home

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -114,7 +118,10 @@ function App() {
 
       <div className="ticks"></div>
       <section id="spacer"></section>
+      
     </>
+
+    
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Navbar from './components/Navbar.jsx'
+import Home from './components/Home'
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Navbar />
+    <Home />
     <App />
     
   </StrictMode>,
